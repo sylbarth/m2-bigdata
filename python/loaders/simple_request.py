3b903f339d92b2f29de3790d146d89f4c8400ea6
Sylvain Barthélémy <sylbarth@gmail.com>
2024-09-04 15:40:36 +0200

    code

---
+++ python/loaders/simple_request.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import requests
+
+# req = requests.get("http://www.lemonde.fr")
+# print(req.text)
+
+
+req = requests.get("http://www.floatrates.com/daily/usd.json")
+data = req.json()
+print(pd.DataFrame(data))
